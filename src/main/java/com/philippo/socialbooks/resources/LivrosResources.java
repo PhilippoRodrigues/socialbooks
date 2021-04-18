d605c5cf8c3812e90597542bd9a0fb969a6ad314
Philippo <philippo.rodrigues@mobicare.com.br>
2021-04-18 17:15:30 -0300

    Adicionando tratamento respostas HTTP 404 e 201

--- src/main/java/com/philippo/socialbooks/resources/LivrosResources.java
+++ src/main/java/com/philippo/socialbooks/resources/LivrosResources.java
@@ -1,17 +1,20 @@
 package com.philippo.socialbooks.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.philippo.socialbooks.domain.Livro;
 import com.philippo.socialbooks.repository.LivrosRepository;
-
 
 @RestController
 @RequestMapping("/livros")
@@ -26,13 +29,24 @@ public class LivrosResources {
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
-	public void salvar(@RequestBody Livro livro) {
-		livrosRepository.save(livro);
+	public ResponseEntity<Void> salvar(@RequestBody Livro livro) {
+		
+		livro = livrosRepository.save(livro);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(livro.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public Livro buscar(@PathVariable("id") Long id) {
-		return livrosRepository.findById(id).orElse(null);
+	public ResponseEntity<?> buscar(@PathVariable("id") Long id) {
+		
+		Livro livro = livrosRepository.findById(id).orElse(null);
+		
+		if (livro == null)
+			return ResponseEntity.notFound().build();
+		
+		return ResponseEntity.status(HttpStatus.OK).body(livro);
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
