9fb9f08386207ddf2e97bf5901beb4cc5c91efcc
Philippo <philippo.rodrigues@mobicare.com.br>
2021-04-24 12:49:49 -0300

    Adicionando exceção e formatando atributos JSON

--- src/main/java/com/philippo/socialbooks/handler/ResourceExceptionHandler.java
+++ src/main/java/com/philippo/socialbooks/handler/ResourceExceptionHandler.java
@@ -2,6 +2,7 @@ package com.philippo.socialbooks.handler;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -56,5 +57,18 @@ public class ResourceExceptionHandler {
 		erro.setTimestamp(System.currentTimeMillis());
 		
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
+	}
+	
+	@ExceptionHandler(DataIntegrityViolationException.class)
+	public ResponseEntity<DetalhesErro> handlerDataIntegrityViolationException(DataIntegrityViolationException e, HttpServletRequest request) {
+		
+		DetalhesErro erro = new DetalhesErro();
+		
+		erro.setStatus(400l);
+		erro.setTitulo("Requisição inválida.");
+		erro.setMensagemDesenvolvedor("http://erros.socialbooks.com/400");
+		erro.setTimestamp(System.currentTimeMillis());
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erro);
 	}
 }
